0bc9323933498a0d74a9964dad50de2818146d1e
HkCode25 <hardeepk_fr@yahoo.fr>
2026-02-28 23:31:06 -0700

    today's changes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,59 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div>Homepage for Little Lemon Restaurant</div>
-    </div>
+    <> className="App"
+<header>
+  <img className="logo" src="logo.jpg" alt="Little Lemon Logo" />
+
+<nav>
+  <ul>
+    <li><a href="#home">Home</a></li>
+    <li><a href="#about">About</a></li>
+    <li><a href="#menu">Menu</a></li>
+    <li><a href="#reservations">Reservations</a></li>
+    <li><a href="#order-online">Order online</a></li>
+    <li><a href="#login">Login</a></li>
+  </ul>
+</nav>
+
+</header>
+
+
+<main>
+  <article>
+    <div>Homepage for Little Lemon Restaurant</div>
+  </article> 
+
+  <article>
+    <div>About section for Little Lemon Restaurant</div>
+  </article>
+
+  <article>
+    <div>Menu section for Little Lemon Restaurant</div>
+  </article>
+
+  <article>
+    <div>Reservations section for Little Lemon Restaurant</div>
+  </article>
+
+</main> 
+
+
+<footer>
+  <nav> 
+    <ul>
+      <li><a href="#home">Home</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#menu">Menu</a></li>
+      <li><a href="#reservations">Reservations</a></li>
+      <li><a href="#order-online">Order online</a></li>
+      <li><a href="#login">Login</a></li>
+    </ul>
+  </nav>
+  <div>Copyright © 2026 Little Lemon Restaurant</div>
+</footer>
+      
+    </>
   );
 }
 
