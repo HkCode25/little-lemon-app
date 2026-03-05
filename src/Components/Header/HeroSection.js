e1cceb2636fc498dafbf7ef4596186f221c6ec7b
HkCode25 <hardeepk_fr@yahoo.fr>
2026-03-04 20:20:08 -0700

    button

--- src/Components/Header/HeroSection.js
+++ src/Components/Header/HeroSection.js
@@ -7,7 +7,7 @@ function HeroSection() {
         <div className='heroCity'><h2>Chicago</h2></div>
         <div className='heroDescription'><p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p></div>
         <div className='heroButton'><button>Reserve a Table</button></div>
-        <div className='heroImage'><img src = {heroimage} width = "400px" alt="Little Lemon Restaurant" /></div>
+        <div className='heroImage'><img src = {heroimage} width = "80%" alt="Little Lemon Restaurant" /></div>
         </section>
     );
 }
