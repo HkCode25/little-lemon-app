d9bf95975f09cb1c9ba0b95564c773e175a2bb8f
HkCode25 <hardeepk_fr@yahoo.fr>
2026-03-15 22:38:07 -0600

    specials

--- src/Components/OrderOnline.js
+++ src/Components/OrderOnline.js
@@ -8,105 +8,54 @@ function OrderOnline () {
     return (
 
     <main className='order-online'>
+      
+      <div className="specials">
+        <div className="title"><h1>This week's specials!</h1></div>
+        <div className='onlineMenu-btn'><button>Online Menu</button></div>
+      </div>
 
-        <div className="title">
-            <h1>This week's specials!</h1>
-            <div className='onlineMenu-btn'><button>Online Menu</button></div>
+      <div className="container swiper mySwiper">
+
+        <div className="card card1 swiper-slide">
+
+          <img src={heroimage} width = "160px" alt="Placeholder image"/>
+          <h3>Greek Salad1 -- $14.99</h3>
+          <p>The famous Greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
+          <div className="cardButton">
+            <button class="btn">Order a delivery</button>
+          </div>
+          <div className="cardIcon">
+            <img src={bikeicon2} alt="bike icon"/>
+          </div>
         </div>
 
-    <div className="container swiper mySwiper">
+        <div className="card card2 swiper-slide">
 
-    <div className="swiper-wrapper">
+          <img src={heroimage} width = "160px" alt="Placeholder image"/>
+          <h3>Greek Salad1 -- $14.99</h3>
+          <p>The famous Greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
+          <div className="cardButton">
+            <button class="btn">Order a delivery</button>
+          </div>
+          <div className="cardIcon">
+            <img src={bikeicon2} alt="bike icon"/>
+          </div>
+        </div>
 
-    <div className="card card1 swiper-slide">
+        <div className="card card3 swiper-slide">
 
-      <img src={heroimage} width = "160px" alt="Placeholder image"/>
-      <h3>Greek Salad1 -- $14.99</h3>
-      <p>The famous Greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
-      <div className="cardButton">
-        <button class="btn">Order a delivery</button>
-      </div>
-      <div className="cardIcon">
-        <img src={bikeicon2} alt="bike icon"/>
-      </div>
-    </div>
+          <img src={heroimage} width = "160px" alt="Placeholder image"/>
+          <h3>Greek Salad1 -- $14.99</h3>
+          <p>The famous Greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
+          <div className="cardButton">
+            <button class="btn">Order a delivery</button>
+          </div>
+          <div className="cardIcon">
+            <img src={bikeicon2} alt="bike icon"/>
+          </div>
+        </div>
 
-
-    <div className="card card2 swiper-slide">
-
-      <img src={heroimage} width = "160px" alt="Placeholder image"/>
-      <h3>Greek Salad1 -- $14.99</h3>
-      <p>The famous Greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
-      <div className="cardButton">
-        <button class="btn">Order a delivery</button>
-      </div>
-      <div className="cardIcon">
-        <img src={bikeicon2} alt="bike icon"/>
-      </div>
-    </div>
-
-
-    <div className="card card3 swiper-slide">
-
-      <img src={heroimage} width = "160px" alt="Placeholder image"/>
-      <h3>Greek Salad1 -- $14.99</h3>
-      <p>The famous Greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
-      <div className="cardButton">
-        <button class="btn">Order a delivery</button>
-      </div>
-      <div className="cardIcon">
-        <img src={bikeicon2} alt="bike icon"/>
-      </div>
-    </div>
-
-
-    <div className="card card4 swiper-slide">
-
-      <img src={heroimage} width = "160px" alt="Placeholder image"/>
-      <h3>Greek Salad1 -- $14.99</h3>
-      <p>The famous Greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
-      <div className="cardButton">
-        <button class="btn">Order a delivery</button>
-      </div>
-      <div className="cardIcon">
-        <img src={bikeicon2} alt="bike icon"/>
-      </div>
-    </div>
-
-    <div className="card card5 swiper-slide">
-
-      <img src={heroimage} width = "160px" alt="Placeholder image"/>
-      <h3>Greek Salad1 -- $14.99</h3>
-      <p>The famous Greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
-      <div className="cardButton">
-        <button class="btn">Order a delivery</button>
-      </div>
-      <div className="cardIcon">
-        <img src={bikeicon2} alt="bike icon"/>
-      </div>
-    </div>
-
-
-    <div className="card card6 swiper-slide">
-
-      <img src={heroimage} width = "160px" alt="Placeholder image"/>
-      <h3>Greek Salad1 -- $14.99</h3>
-      <p>The famous Greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
-      <div className="cardButton">
-        <button class="btn">Order a delivery</button>
-      </div>
-      <div className="cardIcon">
-        <img src={bikeicon2} alt="bike icon"/>
-      </div>
-    </div>
-
-
-    </div>
-        <div class="swiper-button-prev"></div>
-        <div class="swiper-button-next"></div>
-        <div class="swiper-pagination"></div>
-    </div>
-
+</div>
     </main>
 
     );
