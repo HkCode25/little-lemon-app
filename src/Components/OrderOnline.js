082f8df4cca43b9f84317047e91fb2c2531665df
HkCode25 <hardeepk_fr@yahoo.fr>
2026-03-16 13:07:51 -0600

    changes

--- src/Components/OrderOnline.js
+++ src/Components/OrderOnline.js
@@ -7,29 +7,18 @@ function OrderOnline () {
 
     return (
 
-    <main className='order-online'>
       
+
+    <main className='order-online'>
+
       <div className="specials">
-        <div className="title"><h1>This week's specials!</h1></div>
-        <div className='onlineMenu-btn'><button>Online Menu</button></div>
+        <h1 className="title">This week's specials!</h1>
+        <button className='onlineMenu-btn'>Online Menu</button>
       </div>
 
-      <div className="container swiper mySwiper">
+      <div className="container">
 
-        <div className="card card1 swiper-slide">
-
-          <img src={heroimage} width = "160px" alt="Placeholder image"/>
-          <h3>Greek Salad1 -- $14.99</h3>
-          <p>The famous Greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
-          <div className="cardButton">
-            <button class="btn">Order a delivery</button>
-          </div>
-          <div className="cardIcon">
-            <img src={bikeicon2} alt="bike icon"/>
-          </div>
-        </div>
-
-        <div className="card card2 swiper-slide">
+        <div className="card card1">
 
           <img src={heroimage} width = "160px" alt="Placeholder image"/>
           <h3>Greek Salad1 -- $14.99</h3>
@@ -42,7 +31,7 @@ function OrderOnline () {
           </div>
         </div>
 
-        <div className="card card3 swiper-slide">
+        <div className="card card2">
 
           <img src={heroimage} width = "160px" alt="Placeholder image"/>
           <h3>Greek Salad1 -- $14.99</h3>
@@ -55,11 +44,23 @@ function OrderOnline () {
           </div>
         </div>
 
-</div>
-    </main>
+        <div className="card card3">
 
-    );
+          <img src={heroimage} width = "160px" alt="Placeholder image"/>
+          <h3>Greek Salad1 -- $14.99</h3>
+          <p>The famous Greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
+          <div className="cardButton">
+            <button class="btn">Order a delivery</button>
+          </div>
+          <div className="cardIcon">
+            <img src={bikeicon2} alt="bike icon"/>
+          </div>
+        </div>
+
+    </div>
+  </main>
+
+);
 }
-
 
 export default OrderOnline;
