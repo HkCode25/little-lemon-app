0206d51e6f04a4d95adafa99e5700582c12a9b32
HkCode25 <hardeepk_fr@yahoo.fr>
2026-03-17 15:21:11 -0600

    about

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,5 +1,6 @@
 
 import React from "react";
+import heroimage from '../assets/heroimage.jpg';
 import "./About.css";
 
 
@@ -7,9 +8,26 @@ import "./About.css";
 function About() {
   return (
     <div className="about">
-      <div className="about__container">
-        <h1>About section for Little Lemon Restaurant</h1>
+
+    <h1>About</h1>
+
+      <div className="about-container1">
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
       </div>
+
+      <div className="about-container2">
+        <h3>Mario</h3>
+        <p>The famous Greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
+        <img src={heroimage} width = "160px" alt="Placeholder image"/>
+      </div>
+
+      <div className="about-container3">
+        <h3>Adrian</h3>
+        <p>The famous Greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
+        <img src={heroimage} width = "160px" alt="Placeholder image"/>
+      </div>
+
     </div>
   );
 }
