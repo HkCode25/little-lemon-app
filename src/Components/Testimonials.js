1f6cc1023a6da88b7634b9997d7e73c374d72427
HkCode25 <hardeepk_fr@yahoo.fr>
2026-03-16 21:14:24 -0600

    testimonials

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -1,16 +1,79 @@
 import React from "react";
 import './Testimonials.css';
-
+import annaPersona from '../assets/annaPersona.png';
 
 function Testimonials() {
 
+
 return (
-    <div className="testimonials">
-      <div className="testimonials__container">
-        <h1>Testimonials for Little Lemon Restaurant</h1>
-      </div>
+
+    <main className='testimonials'>
+
+      <h1 className="title">Testimonials</h1>
+
+      <div className="container">
+
+        <div className="card card1">
+          <img src={annaPersona} alt="Placeholder image"/>
+          <h3>Ritu Sinh</h3>
+          <h4>Teacher</h4>
+          <p className="starRating">
+            <span>&#9733;</span>
+            <span>&#9733;</span>
+            <span>&#9733;</span>
+            <span>&#9733;</span>
+            <span>&#9734;</span>
+          </p>
+          <p>Great food, service and the staff! Long time neighborhood restaurant.</p>
+        </div>
+
+        <div className="card card2">
+          <img src={annaPersona} alt="Placeholder image"/>
+          <h3>Ritu Sinh</h3>
+          <h4>Teacher</h4>
+          <p className="starRating">
+            <span>&#9733;</span>
+            <span>&#9733;</span>
+            <span>&#9733;</span>
+            <span>&#9733;</span>
+            <span>&#9733;</span>
+          </p>
+          <p>Great food, service and the staff! Long time neighborhood restaurant.</p>
+        </div>
+
+        <div className="card card3">
+          <img src={annaPersona} alt="Placeholder image"/>
+          <h3>Ritu Sinh</h3>
+          <h4>Teacher</h4>
+          <p className="starRating">
+            <span>&#9733;</span>
+            <span>&#9733;</span>
+            <span>&#9733;</span>
+            <span>&#9733;</span>
+            <span>&#9733;</span>
+          </p>
+          <p>Great food, service and the staff! Long time neighborhood restaurant.</p>
+        </div>
+
+        <div className="card card1">
+          <img src={annaPersona} alt="Placeholder image"/>
+          <h3>Ritu Sinh</h3>
+          <h4>Teacher</h4>
+          <p className="starRating">
+            <span>&#9733;</span>
+            <span>&#9733;</span>
+            <span>&#9733;</span>
+            <span>&#9733;</span>
+            <span>&#9734;</span>
+          </p>
+          <p>Great food, service and the staff! Long time neighborhood restaurant.</p>
+        </div>
+
+
     </div>
-  );
+  </main>
+
+);
 }
 
 export default Testimonials;
