77f059935e9320a6b1d8d0c1cc09f790b5b94e23
HkCode25 <hardeepk_fr@yahoo.fr>
2026-03-17 18:30:28 -0600

    footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,32 +1,57 @@
 import logo from '../assets/logo.png';
-import { Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import "./Footer.css";
+import { useRef } from "react";
+import About from "../Components/About";
+import Menu from "../Components/Menu";
+import Reservations from "../Components/Reservations";
+import Home from "../Components/Home";
+import OrderOnline from "../Components/OrderOnline";
+
 
 function Footer() {
 
     return (
-        <footer className='App-footer'>
+    <footer className='App-footer'>
   
-    <Link to="/">
-      <img className='footerLogo' src = {logo} width = "200px" alt="Little Lemon Logo" />
-    </Link>
+        <Link to="/home">
+          <img
+            className="header_logo"
+            src={logo}
+            alt="header_logo"
+          />
+        </Link>
 
 
-    <ul>
-      <li><Link to="/" className="nav-item">Home</Link></li>
-      <li><Link to="/About" className="nav-item">About</Link></li>
-      <li><Link to="/Menu" className="nav-item">Menu</Link></li>
-      <li><Link to="/Reservations" className="nav-item">Reservations</Link></li>
-      <li><Link to="/OrderOnline" className="nav-item">Order online</Link></li>
-      <li><Link to="/Login" className="nav-item">Login</Link></li>
-    </ul>
+      <nav className='footerNav'>       
+          <Link to="/" className="nav-item"><p>Home</p></Link>
+          <Link to="/About" className="nav-item">About</Link>
+          <Link to="/Menu" className="nav-item">Menu</Link>
+          <Link to="/Reservations" className="nav-item">Reservations</Link>
+          <Link to="/OrderOnline" className="nav-item">Order online</Link>
+          <Link to="/Login" className="nav-item">Login</Link>
+      </nav>
 
-    <div>
-    <p>Copyright © 2026 Little Lemon Restaurant</p>
-    </div>
 
-</footer>
-);
-}
+      <div className='footerContact'>
+        <p>Contact Address</p>
+        <p>Phone number </p>
+        <p>Email</p>
+      </div>
+
+
+      <div className='footerSocial'>
+        <p>Social Links</p>
+      </div>
+
+
+      <div className='footerCopyright'>
+      <p>Copyright © 2026 Little Lemon Restaurant</p>
+      </div>
+
+  </footer>
+  );
+  }
 
 export default Footer;
 
