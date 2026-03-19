14aa074179dc53c8df49e778d687138ed282d300
HkCode25 <hardeepk_fr@yahoo.fr>
2026-03-18 19:34:16 -0600

    footer update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,7 +7,7 @@ import Menu from "../Components/Menu";
 import Reservations from "../Components/Reservations";
 import Home from "../Components/Home";
 import OrderOnline from "../Components/OrderOnline";
-
+import { FaYoutube, FaFacebook, FaInstagram } from 'react-icons/fa';
 
 function Footer() {
 
@@ -24,24 +24,29 @@ function Footer() {
 
 
       <nav className='footerNav'>
-          <Link to="/" className="nav-item">Home</Link>
-          <Link to="/About" className="nav-item">About</Link>
-          <Link to="/Menu" className="nav-item">Menu</Link>
-          <Link to="/Reservations" className="nav-item">Reservations</Link>
-          <Link to="/OrderOnline" className="nav-item">Order online</Link>
-          <Link to="/Login" className="nav-item">Login</Link>
+          <Link to="/home" className="nav-item"><p>Home</p></Link>
+          <Link to="/about" className="nav-item">About</Link>
+          <Link to="/menu" className="nav-item">Menu</Link>
+          <Link to="/reservations" className="nav-item">Reservations</Link>
+          <Link to="/orderonline" className="nav-item">Order online</Link>
+          <Link to="/login" className="nav-item">Login</Link>
       </nav>
 
 
       <div className='footerContact'>
-        <p>Contact Address</p>
-        <p>Phone number </p>
+        <p>Contact Address:</p>
+        <p>#1014 17 St SW, Calgary, AB T2D 1T2</p>&nbsp;
+        <p>Phone number: </p>
+        <p>+1 (403) 012-8200 </p>&nbsp;
         <p>Email</p>
+        <p>contact@little-lemon.com</p>
       </div>
 
 
-      <div className='footerSocial'>
-        <p>Social Links</p>
+      <div className='footerSocial social-icons'>
+        <a className='social-icons' href="..."><FaYoutube size={24}/><p>YouTube</p></a>
+        <a className='social-icons' href="..."><FaFacebook size={24}/><p>Facebook</p></a>
+        <a className='social-icons' href="..."><FaInstagram size={24}/><p>Instagram</p></a>
       </div>
 
 
