e8e7d49bd938650c19d86bfbb8811d85e6dbc621
HkCode25 <hardeepk_fr@yahoo.fr>
2026-03-23 14:52:55 -0600

    new changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import './Components/Navbar.css';
 import './Components/Header/heroSection.css';
-/* import logo from './assets/logo.png'; */
 import Navbar from './Components/Navbar';
 import HeroSection from './Components/Header/HeroSection';
 import OrderOnline from './Components/OrderOnline';
@@ -9,15 +8,29 @@ import Footer from './Components/Footer';
 import React from 'react';
 import Testimonials from './Components/Testimonials';
 import About from './Components/About';
+import Menu from "./Components/Menu";
+import Reservations from "./Components/Reservations";
+import Home from "./Components/Home";
+import {Route, Routes} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="AppWrapper">
-        <Navbar />
-        <HeroSection />
-        <OrderOnline />
-        <Testimonials />
-        <About />
+      <Navbar />
+      <HeroSection />
+      <OrderOnline />
+      <Testimonials />
+      <About />
+
+      <Routes>
+        <Route path="/" element={Home} />
+        <Route path="/about" element={About} />
+        <Route path="/menu" element={Menu} />
+        <Route path="/reservations" element={Reservations} />
+        <Route path="/order-online" element={OrderOnline} />
+      </Routes>
+
         <Footer />
     </div>
   );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,14 @@
 import React from "react";
 import logo from '../assets/logo.png';
-import { Routes, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import './Navbar.css';
 import { FaBars, FaTimes } from "react-icons/fa";
 import { useRef } from "react";
-import About from "../Components/About";
+/* import About from "../Components/About";
 import Menu from "../Components/Menu";
 import Reservations from "../Components/Reservations";
 import Home from "../Components/Home";
-import OrderOnline from "../Components/OrderOnline";
+import OrderOnline from "../Components/OrderOnline"; */
 // import Login from "../Components/Login";
 // import App from "../App";
 /* import hamburgerIcon from '../../assets/icons_assets/hamburgerIcon.png'; */
@@ -16,78 +16,34 @@ import OrderOnline from "../Components/OrderOnline";
 
 
 
-function Header() {
-  const navRef = useRef();
-
-  const showNavbar = () => {
-    navRef.current.classList.toggle("responsive_nav");
-  };
+function Navbar() {
 
   return (
     <div className="navbar">
       <header>
-        <Link to="/home">
+        <Link to="/">
           <img
             className="header_logo"
             src={logo}
-            alt="header_logo"
+            alt="header logo"
           />
         </Link>
 
-        <nav ref={navRef}>
-          <Link onClick={showNavbar} to="/home" className="nav-element">
-            <p>Home</p>
-          </Link>
-
-           <Link onClick={showNavbar} to="/about" className="nav-element">
-            <p>About</p>
-          </Link>
-
-          <Link onClick={showNavbar} to="/menu" className="nav-element">
-            <p>Menu</p>
-          </Link>
-
-          <Link onClick={showNavbar} to="/reservations" className="nav-element">
-            <p>Reservations</p>
-          </Link>
-
-          <Link onClick={showNavbar} to="/orderonline" className="nav-element">
-            <p>Order online</p>
-          </Link>
-
-
-          <Link to="/login" className="nav-element">
-            <div className="signin_but">
-              <p>Login</p>
-            </div>
-          </Link> 
-
-          
-          
-
-         
-
-          <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-            <FaTimes />
-          </button>
+        <nav>
+          <ul>
+            <li><Link to="/" className="nav-element">Home</Link></li>
+            <li><Link to="/about" className="nav-element">About</Link></li>
+            <li><Link to="/menu" className="nav-element">Menu</Link></li>
+            <li><Link to="/reservations" className="nav-element">Reservations</Link></li>
+            <li><Link to="/orderonline" className="nav-element">Order online</Link></li>
+            <li><Link to="/login"><div className="signin_but">Login</div></Link></li>
+          </ul>
         </nav>
-
-        <button className="nav-btn" onClick={showNavbar}>
-          <FaBars />
-        </button>
-
-      <Routes> 
-        <Route path="/home" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/menu" element={<Menu />}></Route>
-        <Route path="/reservations" element={<Reservations />}></Route>
-        <Route path="/orderonline" element={<OrderOnline />}></Route>
-      </Routes>
 
       </header>
     </div>
   );
 }
 
-export default Header;
+export default Navbar;
 
