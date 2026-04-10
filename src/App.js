6fe113f2ba16dc469bf46c7076fed003a2b47b7b
HkCode25 <hardeepk_fr@yahoo.fr>
2026-04-10 13:22:21 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -15,8 +15,7 @@ import BookingPage from './Components/BookingPage';
 /* import BookingForm from './Components/BookingForm';
  *//* import { Signup } from './Components/Signup';
 import { Login2 } from './Components/Login2';*/
-import React, { useState } from 'react';
-import { useReducer } from 'react'; 
+import React, { useEffect, useState, useReducer } from 'react';
 import { Routes, Route } from "react-router-dom";
 
 
@@ -26,20 +25,14 @@ import { Routes, Route } from "react-router-dom";
 }  */
 
 
-
-  //this is the function that initializes the available times for the booking form. It returns an array of time slots based on the date selected by the user. If the date falls on a weekend (Saturday or Sunday), it returns a different set of time slots compared to weekdays.
-  export function initializeTimes(dateString) {
+export function initializeTimes(dateString) {
   const date = new Date(dateString);
   const day = date.getDay(); // 0 = Sunday, 6 = Saturday
 
-  if (day === 0 || day === 6) {
-    // Weekend times
-    return ["18:00", "19:00", "20:00", "21:00"];
-  } else {
-    // Weekday times
-    return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+   return day === 0 || day === 6 
+   ? ["18:00", "19:00", "20:00", "21:00"]
+   : ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
   }
-}
 
 
 
@@ -56,12 +49,12 @@ function App() {
     const [availableTimes, dispatch] = React.useReducer(timesReducer, [], initializeTimes);
 
 
-
+    
   return (
     <div className="AppWrapper">
 
       <Navbar />
-         
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
