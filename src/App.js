de7cab53dcf76831709e4e18e26fd3666c298d5a
HkCode25 <hardeepk_fr@yahoo.fr>
2026-04-11 13:43:40 -0600

    newupdate

--- src/App.js
+++ src/App.js
@@ -15,24 +15,31 @@ import BookingPage from './Components/BookingPage';
 /* import BookingForm from './Components/BookingForm';
  *//* import { Signup } from './Components/Signup';
 import { Login2 } from './Components/Login2';*/
-import React, { useEffect, useState, useReducer } from 'react';
+import React, { useState } from 'react';
+import { useReducer } from 'react'; 
 import { Routes, Route } from "react-router-dom";
 
 
 //code for  static times array, which is now replaced by the initializeTimes function that generates the available times based on the date selected by the user.
- /* export function initializeTimes() {
+/* export function initializeTimes() {
   return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
-}  */
+} */
 
 
-export function initializeTimes(dateString) {
+
+  //this is the function that initializes the available times for the booking form. It returns an array of time slots based on the date selected by the user. If the date falls on a weekend (Saturday or Sunday), it returns a different set of time slots compared to weekdays.
+  export function initializeTimes(dateString) {
   const date = new Date(dateString);
   const day = date.getDay(); // 0 = Sunday, 6 = Saturday
 
-   return day === 0 || day === 6 
-   ? ["18:00", "19:00", "20:00", "21:00"]
-   : ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+  if (day === 0 || day === 6) {
+    // Weekend times
+    return ["18:00", "19:00", "20:00", "21:00"];
+  } else {
+    // Weekday times
+    return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
   }
+}
 
 
 
@@ -49,12 +56,12 @@ function App() {
     const [availableTimes, dispatch] = React.useReducer(timesReducer, [], initializeTimes);
 
 
-    
+
   return (
     <div className="AppWrapper">
 
       <Navbar />
-
+         
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
